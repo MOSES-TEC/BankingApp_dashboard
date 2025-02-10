0b214c831228d0b38630722cc4b9a395974a2aa7
MOSES Y <ymoses546@gmail.com>
2025-02-10 21:10:46 +0530

    implement the layout

--- lib/utils.ts
+++ lib/utils.ts
@@ -74,6 +74,17 @@ export function formatAmount(amount: number): string {
 
   return formatter.format(amount);
 }
+/*
+export function formatAmount(amount: number): string { 
+  const formatter = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  });
+
+  return formatter.format(amount);
+}
+*/
 
 export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
 
